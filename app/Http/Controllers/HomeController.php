15a90961da760cb69364a10638035dde0d647e64
Danil Augusto <daniloaugusto0212@hotmail.com>
2020-10-20 21:39:15 -0300

    Controlando acesso - Login e Lougout

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth'); //verifica se o usuario está logado ou não
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     $helloWorld = 'Hello World';
 
     return view('welcome', compact('helloWorld'));
-});
+})->name('home');
 
 Route::get('/model', function(){
     //$products = \App\Product::all(); //select * from products
@@ -112,23 +112,25 @@ Route::get('/model', function(){
 
 
 
-Route::prefix('admin')->name('admin.')->namespace('Admin')->group(function(){
+Route::group(['middleware' => ['auth']], function(){
+    Route::prefix('admin')->name('admin.')->namespace('Admin')->group(function(){
 
-    //Route::prefix('stores')->name('stores.')->group(function(){
+        //Route::prefix('stores')->name('stores.')->group(function(){
 
-     //   Route::get('/', 'StoreController@index')->name('index');
-     //   Route::get('/create', 'StoreController@create')->name('create');
-     //   Route::post('/store', 'StoreController@store')->name('store');
-      //  Route::get('/{store}/edit', 'StoreController@edit')->name('edit');
-      //  Route::post('/update/{store}', 'StoreController@update')->name('update');
-      //  Route::get('/destroy/{store}', 'StoreController@destroy')->name('destroy');
+         //   Route::get('/', 'StoreController@index')->name('index');
+         //   Route::get('/create', 'StoreController@create')->name('create');
+         //   Route::post('/store', 'StoreController@store')->name('store');
+          //  Route::get('/{store}/edit', 'StoreController@edit')->name('edit');
+          //  Route::post('/update/{store}', 'StoreController@update')->name('update');
+          //  Route::get('/destroy/{store}', 'StoreController@destroy')->name('destroy');
 
-   // });
-    Route::resource('stores', 'StoreController');
-    Route::resource('products', 'ProductController');
+       // });
+        Route::resource('stores', 'StoreController');
+        Route::resource('products', 'ProductController');
 
+    });
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');//->middleware('auth');
